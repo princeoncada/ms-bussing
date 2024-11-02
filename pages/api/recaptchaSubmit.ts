89b0d06269122a7e0471ddfc6a71e5b3d7c28f2b
prince <prince.oncada@gmail.com>
2024-11-02 12:00:08 -0400

    dev: code review

--- pages/api/recaptchaSubmit.ts
+++ pages/api/recaptchaSubmit.ts
@@ -1,6 +1,7 @@
 // pages/api/recaptchaSubmit.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+// wtf is going on here?
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         return res.status(200).json({ success: true, message: "API route accessed successfully" });
