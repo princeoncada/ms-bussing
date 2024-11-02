2e761b309e0f4ebccb9ccaf9183c36716ac23832
prince <prince.oncada@gmail.com>
2024-11-02 17:21:20 -0400

    app: tailwind.config.ts changes after shadcn-ui initialization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,91 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "hero": "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('/images/hero.jpg')",
-        "about": "url('/images/about.jpg')",
-        "bus1": "url('/images/bus1.jpg')",
-        "bus2": "url('/images/bus2.jpg')",
-        "bus3": "url('/images/bus3.jpg')",
-        "bus4": "url('/images/bus4.jpg')",
-        "bus5": "url('/images/bus5.jpg')",
-        "bus6": "url('/images/bus6.jpg')",
-      },
-      backgroundSize: {
-        "size-sm": "150%",
-        "size-md": "125%",
-        "size-lg": "120%",
-        "size-about-md": "267%",
-        "size-about-lg": "100%",
-      },
-      backgroundPosition: {
-        "pos-sm": "25% 95%",
-        "pos-md": "18% 70%",
-        "pos-lg": "100% 57%",
-        "pos-about-sm": "25% 65%",
-        "pos-about-md": "22% 65%",
-        "pos-about-lg": "0% 60%",
-      },
-      transitionProperty: {
-        "width": "width",
-      }
-    }
-  },
-  plugins: [],
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'hero': 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("/images/hero.jpg")',
+                'about': 'url("/images/about.jpg")',
+                'bus1': 'url("/images/bus1.jpg")',
+                'bus2': 'url("/images/bus2.jpg")',
+                'bus3': 'url("/images/bus3.jpg")',
+                'bus4': 'url("/images/bus4.jpg")',
+                'bus5': 'url("/images/bus5.jpg")',
+                'bus6': 'url("/images/bus6.jpg")'
+  		},
+            backgroundSize: {
+                'size-sm': '150%',
+                'size-md': '125%',
+                'size-lg': '120%',
+                'size-about-md': '267%',
+                'size-about-lg': '100%'
+            },
+            backgroundPosition: {
+                'pos-sm': '25% 95%',
+                'pos-md': '18% 70%',
+                'pos-lg': '100% 57%',
+                'pos-about-sm': '25% 65%',
+                'pos-about-md': '22% 65%',
+                'pos-about-lg': '0% 60%'
+            },
+            transitionProperty: {
+                'width': 'width'
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            colors: {
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))'
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))'
+                },
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))'
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))'
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))'
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))'
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))'
+                },
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                chart: {
+                    '1': 'hsl(var(--chart-1))',
+                    '2': 'hsl(var(--chart-2))',
+                    '3': 'hsl(var(--chart-3))',
+                    '4': 'hsl(var(--chart-4))',
+                    '5': 'hsl(var(--chart-5))'
+                }
+            }
+        }
+    },
+    plugins: [require("tailwindcss-animate")],
 };
 export default config;
