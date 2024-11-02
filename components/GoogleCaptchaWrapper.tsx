9d28f06e8bc47ad1a558f5c039f472fc19e293a0
prince <prince.oncada@gmail.com>
2024-11-02 17:23:18 -0400

    dev: replaced env variable name

--- components/GoogleCaptchaWrapper.tsx
+++ components/GoogleCaptchaWrapper.tsx
@@ -3,7 +3,7 @@
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function GoogleCaptchaWrapper({ children }: Readonly<{ children: React.ReactNode }>) {
-    const reCaptchaKey: string | undefined = process.env.NEXT_PUBLIC_RECAPTCHA_KEY;
+    const reCaptchaKey: string | undefined = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
     return (
         <GoogleReCaptchaProvider reCaptchaKey={reCaptchaKey ?? "NOT DEFINED"}>
             {children}
